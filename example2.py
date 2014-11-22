78a91b0cae6f226b72a3990eb0333d1573daa910
Trevor Grant <trevor.d.grant@gmail.com>
2014-11-21 22:50:50 -0600

    new example

---
+++ example2.py
@@ -0,0 +1,36 @@
+import requests
+from angemilner import APIKeyLibrarian
+
+l= APIKeyLibrarian()
+PROVIDER = 'google_places'
+
+## To generate keys for google go to https://console.developers.google.com
+google_keys = [	'xxx',
+				'yyy']
+
+
+DAILY_LIMIT = 1000  
+""" 
+Google Places Free Limit, 100,000 if you have a card on file. 
+Divide by 10 if you are doing text searches """
+
+S_BETWEEN_REQUESTS = 0.1 # Technically no limit
+
+for key in google_keys:
+	l.new_api_key(key, PROVIDER, DAILY_LIMIT, S_BETWEEN_REQUESTS )
+
+
+keywords = ['pizza', 'chinese', 'sandwiches', 'beer' ]
+location = '41.9075,-87.6769' # Wicker Park
+
+for q in keywords:
+	url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+	key = l.check_out_api_key(PROVIDER)['key']
+	payload = { 'key' 		: key,
+				'location'	: location, 
+				'radius'	: 1000, 
+				'name'		: 'pizza',
+				'types'		: 'restaurant' }
+	r = requests.get(url, params= payload) 
+	print key	
+	print len(r.json()['results'])
