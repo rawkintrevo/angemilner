f1ffdeb0523618435ddbad0397acc607bb47b2b8
Trevor Grant <trevor.d.grant@gmail.com>
2014-11-22 00:01:41 -0600

    Update setup.py

--- angemilner/setup.py
+++ angemilner/setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1.02'
+version = '0.1.03'
 
 setup(name='angemilner',
       version=version,
       description="Manages a library of API keys using MongoDB to prevent rate limiting",
       long_description="""\
 The Ghost of Ange Milner sets up a library of your API keys using MongoDB.  Then everytime you need to make an API call, she checks out the key that has been resting the longest/ has the most remaining calls before it hits the daily rate limit.""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'License :: OSI Approved :: Apache Software License'
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='Trevor Grant',
       author_email='trevor.d.grant@gmail.com',
@@ -18,7 +21,7 @@ The Ghost of Ange Milner sets up a library of your API keys using MongoDB.  Then
       include_package_data=True,
       zip_safe=True,
       install_requires=[
-          # -*- Extra requirements: -*-
+          "pymongo"
       ],
       entry_points="""
       # -*- Entry points: -*-
